852bf10a4c6f209863049e2867b3638a79d3098f
developer developer <developermagic7@gmail.com>
2021-02-15 11:34:00 -0800

    step_1

--- app/src/main/java/com/example/myhilt/MainActivity.kt
+++ app/src/main/java/com/example/myhilt/MainActivity.kt
@@ -9,5 +9,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        /*
+
+        one of the biggest benefit of dagger/hilt to dagger is that -->
+            dagger/hilt get compile error in other hand dagger get us runtime error
+
+        */
     }
 }
