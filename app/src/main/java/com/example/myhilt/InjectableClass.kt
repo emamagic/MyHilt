6cf2490f654c42c4de35d04c3950187b0277f00e
developer developer <developermagic7@gmail.com>
2021-02-15 11:01:44 -0800

    step_3

--- app/src/main/java/com/example/myhilt/InjectableClass.kt
+++ app/src/main/java/com/example/myhilt/InjectableClass.kt
@@ -2,4 +2,7 @@ package com.example.myhilt
 
 import javax.inject.Inject
 
-class InjectableClass @Inject constructor()
+class InjectableClass @Inject constructor(){
+
+    fun doTask() = " Task Done ... "
+}
--- app/src/main/java/com/example/myhilt/MainActivity.kt
+++ app/src/main/java/com/example/myhilt/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity: AppCompatActivity() {
-
     private val TAG = "MainActivity"
+
+    @Inject
+    lateinit var injectableClass: InjectableClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val task = injectableClass.doTask()
 
+        Log.e(TAG, "onCreate: $task")
 
     }
 
