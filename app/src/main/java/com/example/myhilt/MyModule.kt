c99883140b251cd2071a8958ee137a9fb529f61c
developer developer <developermagic7@gmail.com>
2021-02-19 05:47:06 -0800

    step_7

--- app/src/main/java/com/example/myhilt/MyModule.kt
+++ app/src/main/java/com/example/myhilt/MyModule.kt
@@ -1,13 +1,13 @@
 package com.example.myhilt
 
+import android.content.Context
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ActivityComponent::class)
@@ -20,7 +20,7 @@ object MyModule {
 
     @ActivityScoped
     @Provides
-    fun provideTestInterface(gson: Gson): TestInterface {
+    fun provideTestInterface(gson: Gson ,@ApplicationContext context: Context): TestInterface {
         return ConstructorInjectClass(gson)
     }
 
